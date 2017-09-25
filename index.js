94f33d76b549da5dac65d780455d7b58bcb6042e
Brandon Manke <me@brandonmanke.com>
2017-09-24 19:48:39 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,119 @@
+/**
+ *
+ * @author Brandon Manke
+ * TODO rewrite http promise, to just be able to copy/paste it
+ */
+const https = require('https');
+
+class Destiny2API {
+    constructor(config) {
+        this.host = 'www.bungie.net';
+        this.api = `https://www.bungie.net/Platform/Destiny2`;
+        this.path = '/Platform/Destiny2';
+        this.key = config.key;
+        this.userAgent = config.userAgent || https.globalAgent;
+        this.oauthConfig = {
+            id: config.oauthConfig.id,
+            secret: config.oauthConfig.secret
+        } || {};
+        this.options = {
+            host: this.host,
+            path: '',
+            method: '',
+            headers: {
+                'User-Agent': this.userAgent,
+                'X-API-Key': this.key
+            }
+        }
+    }
+
+    /**
+     * Async GetManifest
+     * Right now it links to sqlite database file, so I don't really know how to handle this as of right now 
+     */
+    getManifest() {
+        this.options.path = '/Platform/Destiny2/Manifest/';
+        this.options.method = 'GET';
+        return new Promise((resolve, reject) => {
+            https.request(this.options, (res) => {
+                const { statusCode } = res;
+                const contentType = res.headers['content-type'];
+                res.setEncoding('utf8');
+                let rawData = '';
+                res.on('data', (chunk) => { rawData += chunk; } );
+                res.on('end', () => {
+                    try {
+                        resolve(rawData);
+                    } catch (err) {
+                        //console.error(err.message);
+                        reject(err.message);
+                    }
+                });
+            }).on('error', (err) => {
+                console.error(`getManifest Error: ${err.message}`);
+                reject(err.message);
+            }).end();
+        });
+    }
+
+    /**
+     *
+     */
+    getDestinyEntityDefinition(typeDefinition, hashIdentifier) {
+        this.options.path = `${this.path}/Manifest/${typeDefinition}/${hashIdentifier}/`;
+        this.options.method = 'GET';
+        return new Promise((resolve, reject) => {
+            https.request(this.options, (res) => {
+                const { statusCode } = res;
+                const contentType = res.headers['content-type'];
+                res.setEncoding('utf8');
+                let rawData = '';
+                res.on('data', (chunk) => { rawData += chunk; } );
+                res.on('end', () => {
+                    try {
+                        //console.log(`Raw Data:\n ${rawData}\n`);
+                        //const parsedData = JSON.parse(rawData);
+                        resolve(rawData);
+                    } catch (err) {
+                        //console.error(err.message);
+                        reject(err.message);
+                    }
+                });
+            }).on('error', (err) => {
+                console.error(`getManifest Error: ${err.message}`);
+                reject(err.message);
+            }).end();
+        });
+    }
+
+    /**
+     * 
+     */
+    getProfile(membershipType, destinyMembershipId) {
+        this.options.path = `/Destiny2/${membershipType}/Profile/${destinyMembershipId}/`;
+        this.options.method = 'GET';
+        return new Promise((resolve, reject) => {
+            https.request(this.options, (res) => {
+                const { statusCode } = res;
+                const contentType = res.headers['content-type'];
+                res.setEncoding('utf8');
+                let rawData = '';
+                res.on('data', (chunk) => { rawData += chunk; } );
+                res.on('end', () => {
+                    try {
+                        //console.log(`Raw Data:\n ${rawData}\n`);
+                        //const parsedData = JSON.parse(rawData);
+                        resolve(rawData);
+                    } catch (err) {
+                        //console.error(err.message);
+                        reject(err.message);
+                    }
+                });
+            }).on('error', (err) => {
+                reject(err.message);
+            }).end();
+        })
+    }
+}
+
+module.exports = Destiny2API;
---
+++ test/test.js
@@ -0,0 +1,22 @@
+// manual test for now
+// will write unit tests once stable version exists
+// https://www.bungie.net/en/Help/Article/45481
+const Destiny2API = require('./index.js');
+const fs = require('fs');
+const config = JSON.parse(fs.readFileSync('./config/apikey.json'));
+const https = require('https');
+
+const destiny = new Destiny2API({
+    key: config.apikey,
+    oauthConfig: {}
+});
+
+destiny.getManifest()
+    .then((res) => {
+        const data = JSON.parse(res);
+    })
+    .catch((error) => {
+        console.error(`Get Manifest Error: ${error}`);
+    });
+//console.log('\n\n====================\n\n');
+//api.getDestinyEntityDefinition('DestinyInventoryItemDefinition', 1346849289);
