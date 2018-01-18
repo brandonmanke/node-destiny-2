984f49a9084cf2a5afec0c981ad295b0f516d17d
Brandon Manke <manke.brandon@gmail.com>
2018-01-18 16:12:22 -0600

    Added incomplete oauth example, version 0.2.3

---
+++ examples/oauth.example.js
@@ -0,0 +1,31 @@
+const Destiny2API = require('../index.js'); // you would use 'node-destiny2' instead
+const fs = require('fs');
+const config = JSON.parse(fs.readFileSync('../config/config.json'));
+
+const destiny = new Destiny2API({
+    key: config.apikey,
+    oauthConfg: {
+        id: config.oauth_client_id,
+        secret: config.oauth_secret_id
+    }
+});
+
+// this would be a example of a sign in url, that should be able to be given by our wrapper
+console.log(`${destiny.oauthConfig.url}?client_id=${config.oauth_client_id}&response_type=code`);
+
+// examples of endpoints with oauth required
+// TODO checks for sign in before calling oauth endpoints
+/*destiny.getLeaderboardsForCharacter(
+    1, 
+    '4611686018452936098', 
+    '2305843009278477570',
+    {
+        maxtop: [10],
+        modes: [7], // story
+    })
+    .then((res) => {
+        console.log(res);
+    })
+    .catch((error) => {
+        console.log(`getLeaderboardsForCharacter Error: ${error}`);
+    });*/
